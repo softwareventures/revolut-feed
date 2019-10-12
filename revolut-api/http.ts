78720926c1e29893af0dd950181e76ce779eb42e
Roxie Gibson <me@roxxers.xyz>
2019-10-12 19:07:18 +0100

    Added a lot more functionality for future use in the HTTPClient. Also added documentation!

--- revolut-api/http.ts
+++ revolut-api/http.ts
@@ -4,7 +4,19 @@ import jwt = require("jsonwebtoken");
 import request = require("request-promise-native");
 import {AccessToken} from "./client";
 
+/**
+ * Basic object definition for get requests' options using the request lib
+ */
+interface Options {
+    headers: { Authorization: string };
+    method: "GET";
+    json: true;
+    url: string;
+}
 
+/**
+ * Class to wrap all http requests with to make code simpler
+ */
 export class HTTPHelper {
     public readonly SUB_DOMAIN: string;
     public readonly API_SUB_DOMAIN: string;
@@ -26,10 +38,17 @@ export class HTTPHelper {
         this.clientAssertionType = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer";
         this.localhost = "127.0.0.1";
     }
-
+    /**
+     * Returns the generic revolut api access consent page for our app
+     * returns string
+     */
     public getAccessUrl(): string {
         return `https://${this.SUB_DOMAIN}.revolut.com/app-confirm?client_id=${this.CLIENT_ID}&redirect_uri=http://${this.localhost}`;
     }
+    /**
+     * Makes request to refresh access token
+     * returns RequestPromise
+     */
     public refreshToken(refreshToken: string): request.RequestPromise {
         const endpoint: string = "auth/token";
         const apiUrl: string = this.API_ROOT + endpoint;
@@ -46,7 +65,10 @@ export class HTTPHelper {
         };
         return request(options);
     }
-
+    /**
+     * Makes request exchange access code for more permanent access and refresh tokens
+     * returns RequestPromise
+     */
     public exchangeAccessCode(authCode: string): request.RequestPromise {
         const endpoint: string = "auth/token";
         const apiUrl: string = this.API_ROOT + endpoint;
@@ -63,16 +85,62 @@ export class HTTPHelper {
         };
         return request(options);
     }
+    /**
+     * Makes request to get all accounts for user
+     * returns RequestPromise
+     */
     public getAccounts(token: AccessToken): request.RequestPromise {
-
         const endpoint: string = "accounts";
-        const apiUrl: string = this.API_ROOT + endpoint;
-        const options = { method: "GET",
-            url: apiUrl,
-            headers: {Authorization: `Bearer ${token.access_token}`},
+        const options = this.createGenericGetOptions(endpoint, token.access_token);
+        return request(options);
+    }
+    /**
+     * Makes request to get counterparty with specified id
+     * returns RequestPromise
+     */
+    public getCounterparty(token: AccessToken, id: string): request.RequestPromise {
+        const endpoint: string = `counterparty/${id}`;
+        const options = this.createGenericGetOptions(endpoint, token.access_token);
+        return request(options);
+    }
+    /**
+     * Makes request to get all counterparties for user
+     * returns RequestPromise
+     */
+    public getCounterparties(token: AccessToken): request.RequestPromise {
+        const endpoint: string = "counterparties";
+        const options = this.createGenericGetOptions(endpoint, token.access_token);
+        return request(options);
+    }
+    /**
+     * Makes request to get transaction with specified id
+     * returns RequestPromise
+     */
+    public getTransaction(token: AccessToken, id: string): request.RequestPromise {
+        const endpoint: string = `transaction/${id}`;
+        const options = this.createGenericGetOptions(endpoint, token.access_token);
+        return request(options);
+    }
+    /**
+     * Makes request to get all transactions for user
+     * returns RequestPromise
+     */
+    public getTransactions(token: AccessToken): request.RequestPromise {
+        const endpoint: string = "transactions";
+        const options = this.createGenericGetOptions(endpoint, token.access_token);
+        return request(options);
+    }
+    /**
+     * Creates generic options for get requests for most of the revolut api
+     * returns Options
+     */
+    private createGenericGetOptions(endpoint: string, accessToken: string): Options {
+        return {
+            method: "GET",
+            url: this.API_ROOT + endpoint,
+            headers: {Authorization: `Bearer ${accessToken}`},
             json: true
         };
-        return request(options);
     }
     /**
      * Creates a json web token (JWT)
