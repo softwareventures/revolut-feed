9fd996774ff0ce5e49d6049b429b48b8d4496fa0
Roxie Gibson <me@roxxers.xyz>
2019-10-14 20:36:41 +0100

    Added commander dependency for argument parsing and cli ux

--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
 
+import program = require("commander");
 import * as dotenv from "dotenv";
 import revolut = require("./revolut-api");
 
+program
+    .option("-d, --debug", "connects and uses the sandbox environment")
+    .option("-f, --from <date>", "date for beginning of transaction search")
+    .option("-t, --to <date>", "date for the end of the transaction search (defaults to 'now' if not given)");
 
+program.parse(process.argv);
+
+console.log(program.debug);
 // Inits .env file, making it accessible in process.env
 dotenv.config();
 
@@ -14,7 +22,7 @@ if (process.env.CLIENT_ID) {
     throw new Error("CLIENT_ID environment variable not set, can't continue...");
 }
 
-const dev: boolean = !!process.env.NODE_DEV;
+const dev = !!program.debug;
 
 const client = new revolut.Client(CLIENT_ID, dev);
 client.authenticate()
